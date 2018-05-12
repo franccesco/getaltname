3d4276e3fe40fc804c75d032a39459cebd8821a4
Franccesco Orozco <francc3sco@gmail.com>
2018-05-11 23:02:15 -0600

    changed welcome message

--- gan_api.py
+++ gan_api.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return index page."""
-    return "Hello World!"
+    return "<center><h1>It works!</h1></center>"
 
 
 @app.route('/<string:host>')
