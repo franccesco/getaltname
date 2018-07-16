42237477de27973184290bf04c53fc95abd19103
Franccesco Orozco <franccesco.orozco@codingdose.info>
2018-07-15 18:34:39 -0600

    restricts python version to 3.4 and up.

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://franccesco.github.io/getaltname/',
     packages=setuptools.find_packages(),
+    python_requires='~=3.4',
     install_requires=[
         'colorama',
         'pyperclip',
